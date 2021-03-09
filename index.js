52dfdefd475016e3b20d260265889fe4a3996ac8
benizubi <benizubi@outlook.com>
2021-03-09 23:23:20 +0000

    Creating db

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const { response } = require('express');
+
 const express = require('express');
 const app = express;
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-const Users = require('./models/product')
+const signUp = require('./models/newUser')
+const signIn = require('./models/oldUser')
 mongoose.connect('mongodb://localhost:27017/dopeboi', { useNewUrlParser: true, useUnifiedTopology: true })
     // shopApp is the name of the database we're going to use   
     .then(() => {
@@ -15,6 +18,8 @@ mongoose.connect('mongodb://localhost:27017/dopeboi', { useNewUrlParser: true, u
         console.log(log("OH no error!!!"))
         console.log(err)
     })
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,7 +29,7 @@ app.use(methodOverride('_method'))
 
 
 app.get('/users', (req, res) => {
-    res.render('users/index')
+    res.render('users/home')
 })
 app.listen(8000, () => {
     console.log("App is listening on port 8000")
---
+++ models/newUser.js
@@ -0,0 +1,28 @@
+//Creating our moddel 
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy']
+    },
+    password: {
+        type: Number || String,
+        required: true,
+        min: 6,
+        max: 30
+    }
+})
+const signUp = mongoose.model('signUp', productSchema);
+// by doing this, it allows us to export and use product on the seed page to create new product
+module.exports = signUp;
