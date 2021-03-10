003ec62733a1759a4bb16acabf9bfa45b8fb5be4
benizubi <benizubi@outlook.com>
2021-03-10 20:11:22 +0000

    slide show updates

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'styles')))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
