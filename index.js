17ea6b936f5ab184ef9c8f506b18fde410a2e92f
benizubi <benizubi@outlook.com>
2021-03-10 20:35:35 +0000

    fixing errors

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'styles')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
