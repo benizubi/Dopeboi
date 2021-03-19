d9302e5f95558bd1291acd98f929d3c7cdc23c2c
benizubi <benizubi@outlook.com>
2021-03-19 06:11:23 +0000

    page Schema

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Requiring allows us to use these framework
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 // const methodOverride = require('method-override');
 const config = require('./config/database');
+
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const expressValidator = require('express-validator');
@@ -19,7 +19,9 @@ db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', function () {
     console.log('Connected to MongoDB');
 });
-// view engine setup
+// init app
+const app = express();
+// view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // Set public /style folder / override method
@@ -58,6 +60,12 @@ app.use(expressValidator({
 
     }
 }));
+// Express Messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
 
 // setting routers 
 const pages = require('./routes/pages.js')
