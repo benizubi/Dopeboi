48611aaca05b2d5fa6867d9ab2751a54c001cf6f
benizubi <benizubi@outlook.com>
2021-03-19 01:48:30 +0000

    Fixed mongoose connection

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const port = 8080;
-// requiring it here to i can connect to the db and linking both pages.
+
+// requiring the files to connect the pages.
 const config = require('./config/database')
+const pages = require('./routes/pages.js')
+const adminPages = require('./routes/adminPage.js');
 
 const signUp = require('./models/newUser')
 const signIn = require('./models/oldUser')
 
-mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true })
+//connect to db
+mongoose.connect(config.database)
+const db = mongoose.connection;
+db
     // shopApp is the name of the database we're going to use   
     .then(() => {
         console.log("Mongo Connection Open!")
@@ -21,21 +26,22 @@ mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: t
         console.log(log("OH no error!!!"))
         console.log(err)
     })
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// this allows us to use ejs
 app.set('view engine', 'ejs');
-
+// Set public /dtyle folder
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'styles')))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-//connecting the routes 
-const pages = require('./routes/pages.js')
 
+app.get('/admin/pages', adminPages);
+app.get('/', pages);
 
+// starting the server here we using port 8080
+const port = 8080;
 app.listen(port, () => {
     console.log("App is listening on port 8080")
 })
--- routes/pages.js
+++ routes/pages.js
@@ -2,29 +2,33 @@ const { Router } = require('express');
 const express = require('express')
 // this allows us to switch between pages using ejs here.
 const router = express.Router();
-module.exports = Router;
 
 
 // account manage //
-app.get('/account', (req, res) => {
+router.get('/account', (req, res) => {
     res.render('users/account')
 })
 // gallery //
-app.get('/gallery', (req, res) => {
+router.get('/gallery', (req, res) => {
     res.render('users/gallery')
 })
 //register //
-app.get('/register', (req, res) => {
+router.get('/register', (req, res) => {
     res.render('users/register')
 })
 // shopping //
-app.get('/shopping', (req, res) => {
+router.get('/shopping', (req, res) => {
     res.render('users/shopping')
 })
 
 
 
 //Home page.//
-app.get('/home', (req, res) => {
-    res.render('users/index',)
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home'
+    });
 })
+
+//Exports
+module.exports = router;
