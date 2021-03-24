1da0f87bffa4ac8e3fcbc95c4f34a5ad9bfdf2b3
benizubi <benizubi@outlook.com>
2021-03-24 06:20:26 +0000

    Account set up

--- routes/pages.js
+++ routes/pages.js
@@ -2,7 +2,12 @@ const express = require('express')
 // this allows us to switch between pages using ejs here.
 const router = express.Router();
 
-
+//  Account
+router.get('/account', (req, res) => {
+    res.render('users/account', {
+        title: 'Account'
+    });
+});
 //  Home page.//
 router.get('/', (req, res) => {
     res.render('index', {
