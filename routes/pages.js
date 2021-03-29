66ac50d049ae86783f2f37084837bc0ca595a0aa
benizubi <benizubi@outlook.com>
2021-03-29 23:57:35 +0100

    pages ejs fix

--- routes/pages.js
+++ routes/pages.js
@@ -27,51 +27,5 @@ router.get('/add-page', function (req, res) {
     });
 });
 
-router.post('/add-page', function (req, res) {
-    // validators  warning message
-    // req.checkBody('title', 'Title must have a value.').notEmpty();
-    req.checkBody('content', 'Content must have a value.').notEmpty();
-
-    const title = req.body.title;
-    const slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-    if (slug == "") {
-        slug = title.replace(/\s+/g, '-').toLowerCase();
-    }
-    const content = req.body.content;
-
-    const errors = req.validationErrors();
-
-    if (errors) {
-        res.render('admin/add_page', {
-            errors: errors,
-            title: title,
-            slug: slug,
-            content: content
-        });
-    } else {
-        Page.findOne({ slug: slug }, function (err, page) {
-            if (page) {
-                req.flash('danger', 'Page slug exists, choose another.');
-                res.render('admin/add_page', {
-                    title: title,
-                    slug: slug,
-                    content: content
-                });
-            } else {
-                const page = new Page({
-                    title: title,
-                    slug: slug,
-                    content: content,
-                    sorting: 100
-                });
-                page.save(function (err) {
-                    if (err) return console.log(err)
-                    req.flash('success', 'Page added!')
-                    res.redirect('/admin/pages');
-                });
-            }
-        });
-    }
-});
 //  Exports //
 module.exports = router;
